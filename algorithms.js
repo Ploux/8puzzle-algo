b18f879c67dff98213c4f4dc87f856c7073afe60
Ploux <peteloux@gmail.com>
2022-04-25 13:41:56 -0500

    add node class framework

--- algorithms.js
+++ algorithms.js
@@ -1,8 +1,10 @@
+// inputs: a puzzle
+
 class Puzzle {
-/* inputs: array of puzzle initial state, integer specifying algorithm to use, max depth if depth limited is chosen
-properties: goal (1, 2, 3, 4, 5, 6, 7, 8, 0)
+/* properties: array of puzzle initial state, integer specifying algorithm to use, max depth if depth limited is chosen
+ goal (1, 2, 3, 4, 5, 6, 7, 8, 0)
 methods: actions (indexes of squares blank can move to), result (what happens after an action), isGoal (puzzle is solved)
-outputs: array of moves to solve puzzle, (possibly time, or # of nodes expanded etc)
+
 
 */
 
@@ -13,3 +15,17 @@ outputs: array of moves to solve puzzle, (possibly time, or # of nodes expanded 
         this.goal = [1, 2, 3, 4, 5, 6, 7, 8, 0];
     }
 }
+
+class Node {
+/* a node in a search tree */
+
+    constructor() {
+      this.state = null;
+      this.parent = null;
+      this.action = null;
+      this.path_cost = null;
+    }
+
+}
+
+// outputs: array of moves to solve puzzle, (possibly time, or # of nodes expanded etc)
