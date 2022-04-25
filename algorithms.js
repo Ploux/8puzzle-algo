e18b31e7ca3d1e6132758ec017d096870f1fe5b6
Peter Loux <prl83@msstate.edu>
2022-04-24 21:49:08 -0500

    create 8 puzzle class

--- algorithms.js
+++ algorithms.js
@@ -1,21 +1,15 @@
-function* geeksforGeeks() {
+class Puzzle {
+/* inputs: array of puzzle initial state, integer specifying algorithm to use, max depth if depth limited is chosen
+properties: goal (1, 2, 3, 4, 5, 6, 7, 8, 0)
+methods: actions (indexes of squares blank can move to), result (what happens after an action), isGoal (puzzle is solved)
+outputs: array of moves to solve puzzle, (possibly time, or # of nodes expanded etc)
 
-  /*expression paused here and return
-  value is undefined as nothing is declared*/
-    yield;
+*/
 
-    //wait for next() to finish
-    gfg(yield "Welcome to GFG");
+    constructor(initial, algo, depth = 0) {
+        this.initial = initial;
+        this.algo = algo;
+        this.depth = depth;
+        this.goal = [1, 2, 3, 4, 5, 6, 7, 8, 0];
+    }
 }
-
-function gfg(x) {
-    console.log("Hello World ", x)
-}
-
-var generator = geeksforGeeks();
-
-//return undefined
-console.log(generator.next());
-
-//return Welcome to GFG
-console.log(generator.next());
